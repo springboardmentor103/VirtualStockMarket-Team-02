9d1061810a697b91e799097e3b8fb95b0c4e120a
ShriramJonwal <s.jonwal1000@gmail.com>
2024-04-23 19:02:15 +0530

    Update Dashboard Page

--- Backend/routes/dashboardRoute.js
+++ Backend/routes/dashboardRoute.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const Purchase = require('../models/Purchase');
+const WatchList = require('../models/WatchList');
 
 // Dashboard Route
 router.post("/dashboard", (req, res) => {
     res.json({msg: "Welcome to the dashboard page"});
 });
+
+// Route to get monthly retail details
+router.get('/monthlyRetail', async (req, res) => {
+    try {
+      // monthly retail detail
+      res.status(200).json(monthlyRetail);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
+  // Route to get purchase information
+router.get('/purchases', async (req, res) => {
+    try {
+      const purchases = await Purchase.find({}, 'userId status volume info');
+      res.status(200).json(purchases);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
+  // Route to get watchList information
+  router.get('/watchList', async (req, res) => {
+    try {
+      const watchList = await WatchList.find({}, 'companyName lastPrice change');
+      res.status(200).json(watchList);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
 module.exports = router;
