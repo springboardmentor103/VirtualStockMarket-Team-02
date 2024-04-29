80f122be4a80c33cfce5f0e77899d6a2acafc7fd
ShriramJonwal <s.jonwal1000@gmail.com>
2024-04-29 22:53:37 +0530

    Update Database File

--- Backend/db.js
+++ Backend/db.js
@@ -3,17 +3,12 @@ require("dotenv").config();
 const mongourl = process.env.MONGO_URL;
 
 const mongodb = async () => {
-  await mongoose
-    .connect(mongourl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(async () => {
-      console.log("MongoDB connected successfully.");
-    })
-    .catch((err) => {
+  try {
+    await mongoose.connect(mongourl);
+      //console.log("MongoDB connected successfully.");
+    } catch (err) {
       console.log(err);
-    });
+    }
 };
 
 module.exports = mongodb;
