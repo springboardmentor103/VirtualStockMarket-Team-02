c96cb93bd0a3698da886e11ff22c0afb67935203
Venu <springboardmentor103@gmail.com>
2024-05-08 15:16:35 +0530

    Added password-changed screen, UI changes

--- Frontend/src/ForgotPassword/ForgotPassword.jsx
+++ Frontend/src/ForgotPassword/ForgotPassword.jsx
@@ -1,35 +1,40 @@
-import React from 'react';
-import "../Login/login.css"
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../Login/login.css";
+import { useNavigate } from "react-router-dom";
 
 function ForgotPassword() {
-    return (
-        <div className="login-container">  
-            <div className="content-wrapper">
-                <div className="login-header">
-                    <div className="header-text">Empowering Your Trades: Where Opportunities Meet Expertise</div>
-                </div>
-                <h2>Forgot Your Password?</h2>
-                <p>Please enter the email you used to sign in</p>
-                <form className="login-form">  
-                    <div className="input-group">
-                        <input type="email" placeholder="Email Address" required />
-                    </div>
-                    <div className="input-group">
-                        <input type="text" placeholder="Enter OTP" />
-                    </div>
-                    <button type="button">
-											<Link to="/reset-password">
-											Request OTP
-											</Link>
-										</button>
-                </form>
-                <div className="signup-link">
-                    Don’t have an account? <a href="/signup">Sign Up</a>
-                </div>
-            </div>
+  const navigate = useNavigate();
+
+  const handleRequestOTP = () => {
+    navigate("/reset-password");
+  };
+  return (
+    <div className="login-container">
+      <div className="content-wrapper">
+        <div className="login-header">
+          <div className="header-text">
+            Empowering Your Trades: Where Opportunities Meet Expertise
+          </div>
         </div>
-    );
+        <h2>Forgot Your Password?</h2>
+        <p>Please enter the email you used to sign in</p>
+        <form className="login-form">
+          <div className="input-group">
+            <input type="email" placeholder="Email Address" required />
+          </div>
+          <div className="input-group">
+            <input type="text" placeholder="Enter OTP" />
+          </div>
+          <button type="button">
+            <div onClick={handleRequestOTP}>Request OTP</div>
+          </button>
+        </form>
+        <div className="signup-link">
+          Don’t have an account? <a href="/signup">Sign Up</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ForgotPassword;
