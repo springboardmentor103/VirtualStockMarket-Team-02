6a75f0aaae499d40403b69cfcac2a9e4832c9b3c
Tejesh Reddy <64296310+tejesh856@users.noreply.github.com>
2024-05-21 16:54:23 +0530

    Update getCoinData.js

--- Backend/Middleware/getCoinData.js
+++ Backend/Middleware/getCoinData.js
@@ -1,6 +1,24 @@
-//getCoinData.js
 const axios = require("axios");
+const getValidSymbols = async () => {
+  const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest`;
+  const headers = {
+    "X-CMC_PRO_API_KEY": process.env.API_KEY,
+  };
 
+  try {
+    const response = await axios.get(url, { headers });
+    const validSymbols = response.data.data.map((coin) => coin.symbol);
+    // const validName = response.data.data.map((coin) => coin.name);
+    return { validSymbols };
+  } catch (error) {
+    throw error.response ? error.response.data : error.message;
+  }
+};
+
+const isCoinValid = async (coin) => {
+  const validCrypto = await getValidSymbols();
+  return validCrypto.validSymbols.includes(coin.toUpperCase());
+};
 const getCoinData = async (coin) => {
   const url = `https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=${coin}`;
   const headers = {
@@ -15,5 +33,4 @@ const getCoinData = async (coin) => {
   }
 };
 
-
-module.exports = { getCoinData };
+module.exports = { getCoinData, isCoinValid };
