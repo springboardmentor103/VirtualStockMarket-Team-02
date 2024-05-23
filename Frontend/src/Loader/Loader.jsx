e96c6f5c1497c5feb1409dbada03f711eeb741a2
Venu <springboardmentor103@gmail.com>
2024-05-23 17:15:50 +0530

    changed Loader

--- Frontend/src/Loader/Loader.jsx
+++ Frontend/src/Loader/Loader.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "./loader.css";
 export default function Loader() {
   return (
-    <div className="loader-container">
-      <div className="loader"></div>
+    <div className="loader-overlay">
+      <div className="loader-box">
+        <div className="loader-circle">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 }
